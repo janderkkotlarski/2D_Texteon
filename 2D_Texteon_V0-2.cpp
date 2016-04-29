479a94ce05e581d975a92d7def42ec46a51f7ede
janderkkotlarski <j.d.kotlarski@gmx.com>
2016-04-29 14:06:14 +0200

    improev

--- 2D_Texteon_V0-2.cpp
+++ 2D_Texteon_V0-2.cpp
@@ -15,7 +15,49 @@
 
 #include "pseudo_random.h"
 
-int show_maze(std::vector <std::vector < int>>& maze)
+int show_maze_line(const std::vector <int>& line, const int side, const char symbol)
+{
+	if (static_cast<int>(line.size()) == side)
+	{
+		std::cout << symbol;
+							
+		for (int count{0}; count < side; ++count)
+		{
+			std::cout << line[count];
+		}
+				
+		std::cout << symbol << '\n';
+		
+		return side;
+	}
+	
+	std::cerr << "Line wrong!\n";
+	
+	return -1;
+}
+
+int show_maze_border(const std::vector <char>& line, const int side, const char symbol)
+{
+	if (static_cast<int>(line.size()) == side)
+	{
+		std::cout << symbol;
+							
+		for (int count{0}; count < side; ++count)
+		{
+			std::cout << line[count];
+		}
+				
+		std::cout << symbol << '\n';
+		
+		return side;
+	}
+	
+	std::cerr << "Border wrong!\n";
+	
+	return -1;
+}
+
+int show_maze(const std::vector <std::vector < int>>& maze)
 {
 	assert(maze.size() > 0);
 	assert(maze.size() == maze[0].size());
@@ -23,12 +65,32 @@ int show_maze(std::vector <std::vector < int>>& maze)
 	const int side_x{static_cast<int>(maze[0].size())};
 	const int side_y{static_cast<int>(maze.size())};
 	
-	if ((static_cast<unsigned int>(radius) == maze.size()) &&
-		(static_cast<unsigned int>(radius) == maze[0].size())
-	{
-		const int side{radius + 1 + radius};
+	const char border_symbol{'+'};	
+	const std::vector <char> border_line(side_x, '-');
+	
+	const char maze_symbol{'|'};
 		
-		for (int count_x{0}; count_x <
+	if (side_x == side_y)
+	{
+		const int radius{(side_x - 1)/2};
+		
+		if (show_maze_border(border_line, side_x, border_symbol) == -1)
+		{
+			return -1;
+		}
+		
+		for (int count_y{0}; count_y < side_y; ++count_y)
+		{							
+			if (show_maze_line(maze[count_y], side_x, maze_symbol) == -1)
+			{
+				return -1;
+			}
+		}
+		
+		if (show_maze_border(border_line, side_x, border_symbol) == -1)
+		{
+			return -1;
+		}
 		
 		return radius;
 	}
@@ -37,7 +99,7 @@ int show_maze(std::vector <std::vector < int>>& maze)
 }
 
 
-int maze_create(std::vector <std::vector < int>>& maze, const int radius)
+int create_maze(std::vector <std::vector < int>>& maze, const int radius)
 {
 	assert(radius > 0);
 	
@@ -59,10 +121,46 @@ int maze_create(std::vector <std::vector < int>>& maze, const int radius)
 	return -1;
 }
 
-void maze_initiate(std::vector <std::vector < int>>& maze, const int radius,
-				   std::vector <unsigned int>& fibonac, const unsigned int fibo_max)
+int maze_initiate(std::vector <std::vector < int>>& maze, const int radius,
+				   std::vector <unsigned int>& fibonac, const unsigned int fibo_max,
+				   const float fraction, const int empty)
 {
+	assert(maze.size() > 0);
+	assert(maze.size() == maze[0].size());
 	
+	const int side_x{static_cast<int>(maze[0].size())};
+	const int side_y{static_cast<int>(maze.size())};
+	
+	if ((side_x == side_y) && ((side_x - 1)/2 == radius))
+	{	
+		for (int count_y{0}; count_y < side_y; ++count_y)
+		{
+			for (int count_x{0}; count_x < side_x; ++count_x)
+			{
+				if ((abs(count_x - radius) > empty) &&
+					(abs(count_y - radius) > empty))
+				{
+					if (((abs(count_x - radius) % 2) == 1) &&
+						((abs(count_y - radius) % 2) == 1))
+					{
+						maze[count_y][count_x] = 1;
+					}
+					
+					if (((abs(count_x - radius) % 2) + (abs(count_y - radius) % 2)) == 1)
+					{
+						if (fraction < fractionize(fibonac, fibo_max))
+						{
+							maze[count_y][count_x] = 1;
+						}
+					}		
+				}				
+			}
+		}
+		
+		return radius;
+	}
+			
+	return -1;
 }
 
 
@@ -77,15 +175,18 @@ int main()
 	std::vector <unsigned int> fibo{random_initializer(fibo_max)};	
 	show_fibo(fibo);
 	
+	const int radius{3};
 	
+	std::vector <std::vector < int>> maze;
 		
-	fibo_fix(fibo, fibo_max);	
-	show_fibo(fibo);
-	
-	for (int count{0}; count < 10; ++count)
+	if (create_maze(maze, radius) == -1)
 	{
-		pseudonacci(fibo, fibo_max);
-		show_fibo(fibo);
+		return 1;
+	}
+	
+	if (show_maze(maze) == -1)
+	{
+		return 1;
 	}
 	
 	std::cout << program_name << '\n';
